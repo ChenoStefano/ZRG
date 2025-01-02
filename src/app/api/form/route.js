b3c37a25f1c0e1f6e99b191bca82ca53244d99cd
ChenoStefano <sjcheno@hotmail.com>
2025-01-02 20:10:21 -0300

    apy key

--- src/app/api/form/route.js
+++ src/app/api/form/route.js
@@ -1,7 +1,7 @@
 import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_RTFFxpUE_NejsWkd5uPf97319WLhEbiF3');
 
 export async function POST(request) {
   try {
